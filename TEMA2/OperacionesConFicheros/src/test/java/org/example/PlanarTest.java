eae6e4c8388b77928f26b5014fe1c65197d0893e
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-02 14:15:18 +0200

    Cuarto commit
    Documentado el testPlanar()

--- TEMA2/OperacionesConFicheros/src/test/java/org/example/PlanarTest.java
+++ TEMA2/OperacionesConFicheros/src/test/java/org/example/PlanarTest.java
@@ -10,6 +10,12 @@ import org.mockito.Mockito;
 public class PlanarTest {
 
 
+    /**
+     * Test method for {@link org.example.Planar#planar()}.
+     * This test is used to test the method planar() in the class Planar.
+     *
+     * @see org.example.Planar#planar()
+     */
     @Test
     public void testPlanar() {
         Planar planar = Mockito.mock(Planar.class);
