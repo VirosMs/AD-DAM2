7e802b9c918a00a2f635a055927a15bebe27e491
chaarrsan <charlestacodeouro@hotmail.com>
2024-02-15 19:37:54 +0100

    Clonando proyecto

--- TEMA4/SpringF1/src/main/java/org/api/springf1/exception/ConstructorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.api.springf1.exception;
-
-public class ConstructorNotFoundException extends RuntimeException {
-    public ConstructorNotFoundException(String message) {
-        super(message);
-    }
-}
--- TEMA4/SpringF1/src/main/java/org/api/springf1/exception/DriverNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.api.springf1.exception;
-
-public class DriverNotFoundException extends RuntimeException {
-    public DriverNotFoundException(String message) {
-        super(message);
-    }
-}
